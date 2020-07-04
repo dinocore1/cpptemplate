3f7ec32045f4e5c50af897895c8f154229b5288c
Paul S <paul@PaulSouyMacbook.home.sproutlyled.com>
2020-07-03 21:34:00 -0400

    wip

--- makelib/definitions.mk
+++ makelib/definitions.mk
@@ -8,8 +8,32 @@ LOCAL_CPP_FILES = \
 LOCAL_C_FILES = \
 	$(sort $(strip $(filter %.c,$(SRC_FILES))))
 
+OBJ = $(LOCAL_BUILD_DIR)/$(addsuffix .o,$(notdir $(SRC)))
+
 define src_to_obj
 $(foreach file,$(LOCAL_SRCS),$(LOCAL_BUILD_DIR)/$(addsuffix .o,$(notdir $(file))))
+endef
+
+define compile_c
+$(eval LOCAL_OBJS += $(OBJ))
+$(OBJ): $(SRC)
+	$(CC) -c $(LOCAL_C_FLAGS) -o $@ $^
+
+endef
+
+define compile_cpp
+$(eval LOCAL_OBJS += $(OBJ))
+$(OBJ): $(SRC)
+	$(CC) -c $(LOCAL_CXX_FLAGS) -o $@ $^
+
+endef
+
+define compile
+
+$(foreach SRC,$(LOCAL_C_FILES),$(call compile_c))
+
+$(foreach SRC,$(LOCAL_CPP_FILES),$(call compile_cpp))
+
 endef
 
 define newmodule
@@ -22,15 +46,19 @@ endef
 
 define module
 $(eval ALL_MODULES += $(MODULE))
+$(eval LOCAL_C_FLAGS := $(C_FLAGS))
 $(eval LOCAL_SRCS := $(LOCAL_C_FILES) $(LOCAL_CPP_FILES))
 $(eval LOCAL_BUILD_DIR := $(BUILD_DIR)/$(MODULE))
-$(eval LOCAL_OBJS := $(call src_to_obj))
+$(eval LOCAL_OBJS := )
+
+$(call compile)
+
 endef
 
 define build_exe
 $(eval $(call module))
 $(info module=$(MODULE))
-$(info src files $(LOCAL_SRCS))
-$(info objs $(LOCAL_OBJS))
+$(info src files=$(LOCAL_SRCS))
+$(info objs=$(LOCAL_OBJS))
 endef
 
